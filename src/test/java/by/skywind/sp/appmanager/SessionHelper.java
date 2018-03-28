b20e80ca29cfa9a909f5759fe24243a711d6c31b
Stasnislav Peshkur <stasnislav.peshkur@Stasnislavs-MBP.localdomain>
2018-03-28 17:16:22 +0300

    refactoring

--- src/test/java/by/skywind/sp/appmanager/SessionHelper.java
+++ src/test/java/by/skywind/sp/appmanager/SessionHelper.java
@@ -17,21 +17,12 @@ public class SessionHelper extends HelperBase {
     }
 
     public void login(LoginData loginData){
-        //WebDriverWait wait=new WebDriverWait(driver, 20);
-        //wait.until(ExpectedConditions.visibilityOf(By.xpath( "//div[@class='form-group']/textarea[1]"), loginData.getManagerCredential()));
-        //type(By.xpath(""), loginData.getManagerCredential());
-        //wait(By.xpath("//div[@class='form-group']/textarea[1]"));
         wait(By.xpath("//div[@class='form-group']/textarea[1]"));
         type(By.xpath("//div[@class='form-group']/textarea[1]"), loginData.getManagerCredential());
-
         wait(By.xpath("//button[@class='btn btn-primary']"));
         click(By.xpath("//button[@class='btn btn-primary']"));
-
         try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
-
-        wait(By.xpath("//form[2]/button[@class='btn btn-success']"));
         click(By.xpath("//form[2]/button[@class='btn btn-success']"));
-
         wait(By.id("sign_username"));
         type(By.id("sign_username"), loginData.getLogin());
         type(By.id("sign_password"), loginData.getPassword());
